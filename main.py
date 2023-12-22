ba101f232369d4e8a1f9a21f262abc36291dc9d7
Meirkhan Rakhmetzhanov <meirkhan.rakhmetzhanov@converteo.com>
2023-12-22 18:41:00 +0100

    math operations created

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def function_a(a,b):
-    return a + b
+from operations import add, subtract
 
-def function_b(c,d):
-    return c * d * 100
+def main():
+    print("Math Operations")
+    x, y = 10, 5
+
+    print(f"{x} + {y} = {add(x, y)}")
+    print(f"{x} - {y} = {subtract(x, y)}")
+
+if __name__ == "__main__":
+    main()
