de915130d5fa5dc6596913980c970c078fadc384
Meirkhan Rakhmetzhanov <meirkhan.rakhmetzhanov@converteo.com>
2024-01-16 10:32:52 +0100

    base pipeline redone and flake8 linter added

--- arithmetics/tests/test_operations.py
+++ arithmetics/tests/test_operations.py
@@ -1,23 +1,19 @@
-import unittest
 from arithmetics.operations import add, subtract, multiply
 
-class TestArithmetics(unittest.TestCase):
-    
-    def test_add(self):
-        self.assertEqual(add(5, 3), 8)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(0, 0), 0)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(10, 5, 3), 2)
-        self.assertEqual(subtract(5, 3, 2), 0)
-        self.assertEqual(subtract(-3, -2, -1), 0)
+def test_add():
+    assert add(5, 3) == 8
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
-    def test_multiply(self):
-        self.assertEqual(multiply(3, 4), 12)
-        self.assertEqual(multiply(-1, 5), -5)
-        self.assertEqual(multiply(0, 10), 0)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_subtract():
+    assert subtract(10, 5, 3) == 2
+    assert subtract(5, 3, 2) == 0
+    assert subtract(-3, -2, -1) == 0
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(-1, 5) == -5
+    assert multiply(0, 10) == 0
